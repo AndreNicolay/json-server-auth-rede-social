dc2e610564a0291613b6b2d15743c60ab7d461b0
ArthurTech8 <iarthurti@gmail.com>
2026-09-25 20:12:09 -0300

    Interval

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 // Direcionamento de porta para servidor online
-setinterval(() => {
-console.log('Servidor rodando na porta 3000');
+setInterval(() => {
+  console.log('Servidor rodando na porta 3000');
 }, 30000);
